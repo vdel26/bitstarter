0bcc388d7c6f2113b980eb4c3673ce22b5b0b195
Victor Delgado <vdel26@gmail.com>
2013-07-09 08:58:54 +0200

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var text = fs.readFileSync('index.html').toString('String');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
